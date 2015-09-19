47ed2ce887e646dc00afc59595c7112de186c154
kevroy314 <kevin.horecka@gmail.com>
2015-09-18 19:20:38 -0500

    Added form validation
    -Added form validation with red X marks next to each item when they are
    invalid
    -Added iTween library for visualization of validation
    -Added punch-shake move to input elements which are invalid when form is
    submitted
    -Added two types of validation, non-empty string enforcement, and
    generalized date-time format (and validity) enforcement
    
    Next things on form:
    -Add tabbing between form elements
    -Add a "test mode" button which allows populates a bunch of semi-random
    test inputs for faster testing of main program
    -Populate form data to player preferences
    
    Then it's on to the task!

---
+++ Assets/InputFieldDateTimeFormatEnforcer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldDateTimeFormatEnforcer : MonoBehaviour {
+
+    private InputField inputText;
+    private GameObject errorObject;
+
+    public string dateTimeFormat = "dd/MM/yy";
+    public string errorObjectName = "Error";
+    public string cultureInfoString = "en-US";
+    public System.Globalization.DateTimeStyles dateTimeStyles = System.Globalization.DateTimeStyles.None;
+
+    private bool valid = false;
+
+	// Use this for initialization
+	void Start () {
+        inputText = gameObject.GetComponent<InputField>();
+        foreach (Transform child in transform)
+            if (child.name == errorObjectName)
+                errorObject = child.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        System.DateTime inputDateTime;
+        valid = System.DateTime.TryParseExact(inputText.text, dateTimeFormat, new System.Globalization.CultureInfo(cultureInfoString), dateTimeStyles, out inputDateTime);
+        errorObject.SetActive(!valid);
+	}
+
+    public bool isValid()
+    {
+        return valid;
+    }
+}
---
+++ Assets/InputFieldNotEmptyEnforcer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldNotEmptyEnforcer : MonoBehaviour {
+
+    private InputField inputText;
+    private GameObject errorObject;
+
+    public string errorObjectName = "Error";
+
+    private bool valid = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        inputText = gameObject.GetComponent<InputField>();
+        foreach (Transform child in transform)
+            if (child.name == errorObjectName)
+                errorObject = child.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        valid = inputText.text.Trim() != "";
+        errorObject.SetActive(!valid);
+    }
+
+    public bool isValid()
+    {
+        return valid;
+    }
+}
